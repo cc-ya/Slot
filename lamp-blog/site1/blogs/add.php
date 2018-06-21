161e314511410763b529c95258cc3d3aa7a5f637
yano <x20a1224@gmail.com>
2018-06-21 20:38:17 +0900

    .DS_Storeの削除

--- lamp-blog/site1/blogs/add.php
+++ lamp-blog/site1/blogs/add.php
@@ -1,9 +1,6 @@
-<?php
-
-require_once("../common/base.php");
-echo __FILE__ . '<br />';
-
+<?php require_once("../common/base.php");
+echo __FILE__. '<br />';
 $base = new Base();
 $base -> titleDisplay();
-
+$base -> headerDisplay();
 ?>
--- lamp-blog/site1/common/base.php
+++ lamp-blog/site1/common/base.php
@@ -5,23 +5,32 @@ class Base
     {
 		ini_set('display_errors', 1);
 		error_reporting(E_ALL);
-		echo "コンスト";
     }
 
 	public function titleDisplay()
 	{
-		echo 'aaaa';
+?>
+		<!DOCTYPE html>
+		<html lang="jp">
+		<head>
+		    <meta charset="utf-8">
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<title>LAMPブログ</title>
+		</head>
+		</html>
+<?php
+	}
+	public function headerDisplay()
+	{
+?>
+		<body>
+			*********************
+			<h1>ブログタイトル</h1>
+			<br>
+			*********************
+		</body>
+<?php
 	}
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="jp">
-<head>
-    <meta charset="utf-8">
-	<title>タイトル</title>
-</head>
-<body>
-	<h1>ブログタイトル</h1>
-</body>
-</html>
